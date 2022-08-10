d0475afd238f2d8f6b8e2f25a40834f307c9ff3a
Julia <jskredova@yandex.ru>
2022-08-10 23:47:52 +0300

    add Task 010 Three Digit

---
+++ Tasks/Task010_ThreeDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number = ReadInt("Введите трёхзначное число: ");
+int firstDigit = number / 100;
+int secondDigit = number / 10;
+
+int secondNumber = secondDigit - firstDigit * 10; 
+Console.WriteLine(secondNumber);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
