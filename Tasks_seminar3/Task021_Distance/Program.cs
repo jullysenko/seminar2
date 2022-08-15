cc03234a8c8f05f4c7eb6e22f26c3ed7466c17cd
Julia <jskredova@yandex.ru>
2022-08-15 23:16:16 +0300

    add Task 021 Distance

---
+++ Tasks_seminar3/Task021_Distance/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = ReadInt("Введите X точки A: ");
+int y1 = ReadInt("Введите Y точки A: ");
+int z1 = ReadInt("Введите Z точки A: ");
+int x2 = ReadInt("Введите X точки B: ");
+int y2 = ReadInt("Введите Y точки B: ");
+int z2 = ReadInt("Введите Z точки B: ");
+
+int lengthX = x2 - x1;
+int lengthY = y2 - y1;
+int lengthZ = z2 - z1;
+double distance = Math.Sqrt(lengthX * lengthX + lengthY * lengthY + lengthZ * lengthZ);
+Console.WriteLine($"A({x1}, {y1}, {z1}); B({x2}, {y2}, {z2}) -> {distance}");
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
