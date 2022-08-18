408beb039037c4ae37e8799f59b3302555aa1adc
Julia <jskredova@yandex.ru>
2022-08-18 23:46:41 +0300

    add Task 027 Sum

---
+++ Tasks_seminar4/Task027_Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿//                                                Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+// while (number > 0)
+// {
+//    int num = number % 10;
+//    number = number / 10;
+//     sum = sum + num;
+//  }
+
+// Console.WriteLine(sum);
+
+
+int Sum(int count)
+{
+    while (count > 0)
+{
+   int num = count % 10;
+   count = count / 10;
+    sum = sum + num;
+ }
+return sum;
+}
+int result = Sum(number);
+Console.WriteLine(result);
+
