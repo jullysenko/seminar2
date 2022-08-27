3d1205aada2bc694d2f324b10b2a60a32d61541c
Julia <jskredova@yandex.ru>
2022-08-27 20:21:18 +0300

    add Task 047 DimensionalArray

---
+++ Tasks_seminar7/Task047_TwoDimensionalArray/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double [m,n];
+
+void PrintArray (double[,] matr)
+{
+    for (int i=0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j < matr.GetLength(1); j++)
+            {
+            Console.Write ($"{matr[i,j]}  ");  
+            }
+            Console.WriteLine();
+    }
+}
+
+void FillArray (double[,] matr)
+    {
+        for (int i=0; i < matr.GetLength(0); i++)
+        {
+            for (int j=0; j < matr.GetLength(1); j++)
+                {
+                matr[i,j] = Math.Round(new Random().NextDouble() * 10, 1);  
+                }
+        }
+    }
+
+FillArray(matrix);
+PrintArray(matrix);
+
