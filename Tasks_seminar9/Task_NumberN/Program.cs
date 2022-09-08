5b4556e5e14ad8e9a2b53703afe3a7f772b99864
Julia <jskredova@yandex.ru>
2022-09-08 23:57:34 +0300

    add Task Number N

---
+++ Tasks_seminar9/Task_NumberN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 1;
+
+string NumAB(int numA, int numB)
+{
+    if (numA <= numB)
+    {
+        return NumAB(numA + 1, numB) + $"{numA}\t";
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+Console.WriteLine(NumAB(numberB, numberA));
+
