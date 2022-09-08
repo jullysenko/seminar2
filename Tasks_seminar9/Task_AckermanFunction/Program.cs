f8503f09270a3dd20f1752fd4e1072baf6920b87
Julia <jskredova@yandex.ru>
2022-09-08 23:56:10 +0300

    add Task Ackerman Function

---
+++ Tasks_seminar9/Task_AckermanFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int numberM = ReadInt("Введите число M: ");
+int numberN = ReadInt("Введите число N: ");
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0 && m > 0)
+    {
+        return Ackerman(m - 1, 1);
+    }
+    else
+    {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+
+}
+Console.WriteLine(Ackerman(numberM, numberN));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
