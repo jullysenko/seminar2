f889a3d412ea6f4aec80a05073a5e8f58c0131ec
Julia <jskredova@yandex.ru>
2022-09-08 23:58:03 +0300

    add Task Number N M

---
+++ Tasks_seminar9/Task_NumberNM/Program.cs
@@ -0,0 +1,37 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int numberN = ReadInt("Введите число N: ");
+int numberM = ReadInt("Введите число M: ");
+
+string NumAB(int m, int n)
+{
+    if (m <= n)
+    {
+        return $"{m}\t" + NumAB(m + 1, n);
+    }
+    return String.Empty;
+}
+
+if (numberM < numberN)
+{
+    Console.WriteLine(NumAB(numberM, numberN));
+}
+if (numberN < numberM)
+{
+    Console.WriteLine(Num(numberN, numberM));
+}
+
+string Num(int n, int m)
+{
+    if (n <= m)
+    {
+        return $"{m}\t" + Num(n, m - 1);
+    }
+    return String.Empty;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
