52d76e28d28aad2979a82bf6cd1699302dac24a9
Julia <jskredova@yandex.ru>
2022-09-08 23:58:39 +0300

    add Task Sum

---
+++ Tasks_seminar9/Task_Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int numberN = ReadInt("Введите число N: ");
+int numberM = ReadInt("Введите число M: ");
+
+int Sum(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return m + Sum(m + 1, n);
+}
+
+if (numberM < numberN)
+{
+    Console.WriteLine(Sum(numberM, numberN));
+}
+if (numberN < numberM)
+{
+    Console.WriteLine(Sum(numberN, numberM));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
