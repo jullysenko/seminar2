46fae4e8a3a3e47706970ec77928ae89f727138f
Julia <jskredova@yandex.ru>
2022-09-08 23:56:54 +0300

    add Task First Numbers

---
+++ Tasks_seminar9/Task_FirstNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+int element1 = ReadInt("Введите первый элемент: ");
+int element2 = ReadInt("Введите второй элемент: ");
+int n = ReadInt("введите количество элементов N: ");
+
+int[] fibonachi = new int[n];
+
+fibonachi[0] = element1;
+fibonachi[1] = element2;
+
+for(int i = 2; i < n; i++)
+{
+    fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+}
+// Array(fibonachi);
+Console.Write(string.Join(" ", fibonachi));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// void Array(int[] arr, int i)
+// {
+//     i = 2;
+//     if (i < arr.Length)
+//     {
+//         arr[i] = arr[i - 1] + arr[i - 2];
+//         Array(arr, ++i);
+//     }
+//     else return;
+// }
